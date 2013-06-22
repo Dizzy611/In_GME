7eb23ceb6a5ad18ce92e37808d4e1e8090d97e7f
Dylan Morrison <insidious@gmail.com>
2013-06-22 17:14:40 -0400

    Added binary of debug version, fixed build scripts.

--- in_gme_build.sh
+++ in_gme_build.sh
@@ -1,4 +1,4 @@
-rm *.dll *.o
+rm in_gme.dll *.o
 gcc -I./libgme/include/gme/ -I/c/Program\ Files\ \(x86\)/Winamp\ SDK/Winamp/ -c MAIN_gme.c
 g++ -mdll -static -static-libgcc -static-libstdc++ -L./libgme/lib/ MAIN_gme.o -lgme -o in_gme.dll
 
--- in_gme_debug_build.sh
+++ in_gme_debug_build.sh
@@ -1,4 +1,4 @@
-rm *.dll *.o
+rm in_gme_debug.dll *.o
 gcc -DDEBUG -I./libgme/include/gme/ -I/c/Program\ Files\ \(x86\)/Winamp\ SDK/Winamp/ -g -c MAIN_gme.c
 g++ -mdll -static -static-libgcc -static-libstdc++ -L./libgme/lib/ MAIN_gme.o -lgme -g -o in_gme_debug.dll
 
