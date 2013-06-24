9f28eeec6e39e1b3a64bf1c030955c7433c1ba27
Dylan Morrison <insidious@gmail.com>
2013-06-23 23:06:33 -0400

    Had to disable preprocessor warnings to allow build without filling my screen due to redefinitions in resource.h: See extended.
    The resource.h VS2005 generated redefines a lot of things defined in
    windows.h (and, I checked, defines them to be the same as what windows.h
    defines them as). Until I'm done working on the dialogs it's going to
    need to stay that way. After I am done, I can remove the definitions
    that are being redefined. So as much as I'm uncomfortable with turning
    off all preprocessor warnings, it'll have to be that way in the
    experimental branch until I'm done with this bit.

--- in_gme_build.sh
+++ in_gme_build.sh
@@ -1,5 +1,5 @@
 rm -f in_gme.dll *.o ./resources/*.o
-gcc -I./libgme/include/gme/ -I./resources/ -I/c/Program\ Files\ \(x86\)/Winamp\ SDK/Winamp/ -c MAIN_gme.c
+gcc -I./libgme/include/gme/ -I./resources/ -I/c/Program\ Files\ \(x86\)/Winamp\ SDK/Winamp/ -Wp,-w -c MAIN_gme.c
 windres -i ./resources/resource.rc -o ./resources/resource.o
 g++ -mdll -static -static-libgcc -static-libstdc++ -L./libgme/lib/ MAIN_gme.o ./resources/resource.o -lgme -o in_gme.dll
 rm -f *.o ./resources/*.o
