1e1fcc97ef36943126325a225c2e1d618e1b23c6
Dylan Morrison <insidious@gmail.com>
2013-06-23 22:23:45 -0400

    First work on integrating resources, which will be necessary for the about/config/info/multi-track dialog boxes.

--- in_gme_build.sh
+++ in_gme_build.sh
@@ -1,4 +1,5 @@
-rm in_gme.dll *.o
+rm -f in_gme.dll *.o ./resources/*.o
 gcc -I./libgme/include/gme/ -I/c/Program\ Files\ \(x86\)/Winamp\ SDK/Winamp/ -c MAIN_gme.c
-g++ -mdll -static -static-libgcc -static-libstdc++ -L./libgme/lib/ MAIN_gme.o -lgme -o in_gme.dll
-
+windres -i ./resources/resource.rc -o ./resources/resource.o
+g++ -mdll -static -static-libgcc -static-libstdc++ -L./libgme/lib/ MAIN_gme.o ./resources/resource.o -lgme -o in_gme.dll
+rm -f *.o ./resources/*.o
---
+++ resources/resource.h
@@ -0,0 +1,2 @@
+#define IDD_ABOUTDIALOG                 103
+#define IDC_STATIC -1
